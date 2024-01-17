0ea5eff4afe6a00fa8ae55bbd09f08e415d2fad1
vandkurnia <vandkurnia@gmail.com>
2024-01-17 17:52:53 +0700

    p

--- app/Http/Controllers/Api/ApiPelangganController.php
+++ app/Http/Controllers/Api/ApiPelangganController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Pelanggan;
 use App\Http\Resources\PostResource;
+use App\Models\Pesanan;
 use App\Models\Tagihan;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Carbon\Carbon;
 
 
 class ApiPelangganController extends Controller
 {
-    public function index($id){
+    public function index($id)
+    {
         $pelanggan = Pelanggan::find($id);
-    
-        if($pelanggan){
+
+        if ($pelanggan) {
             return new PostResource(true, 'Get Berhasil', $pelanggan);
         } else {
             return response()->json(["message" => "Not Found 404"], 404);
@@ -42,13 +45,23 @@ class ApiPelangganController extends Controller
 
         // Verifikasi password
         if (password_verify($request->password, $pelanggan->password)) {
-            $token = $pelanggan->createToken('myappToken')->plainTextToken;
+            // Periksa apakah pengguna sudah login di perangkat lain
+            if ($pelanggan->tokens()->count() > 0) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Pengguna sudah masuk di perangkat lain.',
+                ], 422);
+            } else {
 
-            return response()->json([
-                'success' => true,
-                'token' => $token,
-                'datauser' => $pelanggan,
-            ], 200);
+                // Buat token untuk perangkat saat ini
+                $token = $pelanggan->createToken('myappToken')->plainTextToken;
+
+                return response()->json([
+                    'success' => true,
+                    'token' => $token,
+                    'datauser' => $pelanggan,
+                ], 200);
+            }
         } else {
             return response()->json([
                 'success' => false,
@@ -95,16 +108,16 @@ class ApiPelangganController extends Controller
         }
     }
 
-    public function edit_index(string $id){
+    public function edit_index(string $id)
+    {
         $pelanggan = Pelanggan::where('id_pelanggan', $id)->first();
-    
+
         if (empty($pelanggan)) {
             return response()->json([
                 'success' => false,
                 'message' => 'Data tidak ditemukan!',
             ], 422);
-        }
-        else{
+        } else {
             $pelanggan->no_hp = $this->hidePhoneNumber($pelanggan->no_hp);
             $pelanggan->email = $this->encryptEmail($pelanggan->email);
             return response()->json([
@@ -114,8 +127,9 @@ class ApiPelangganController extends Controller
             ], 200);
         }
     }
-    
-    public function edit_action(string $id, Request $request){
+
+    public function edit_action(string $id, Request $request)
+    {
         try {
             $request->validate([
                 'nama' => 'required|string|max:255',
@@ -123,7 +137,7 @@ class ApiPelangganController extends Controller
                 'alamat' => 'required|string|max:255',
                 'no_hp' => 'required|string|max:15',
             ]);
-    
+
             $pelanggan = Pelanggan::find($id);
             if (empty($pelanggan)) {
                 return response()->json([
@@ -131,18 +145,18 @@ class ApiPelangganController extends Controller
                     'message' => 'Data tidak ditemukan!',
                 ], 422);
             }
-    
+
             $pelanggan->nama = $request->input('nama');
             $pelanggan->email = $request->input('email');
-            $pelanggan->no_hp = $request->input('no_hp');        
+            $pelanggan->no_hp = $request->input('no_hp');
             $pelanggan->alamat = $request->input('alamat');
             $pelanggan->save();
-    
+
             return response()->json([
                 'success' => true,
                 'message' => 'Data berhasil diubah',
                 'datauser' => $pelanggan,
-            ], 200);    
+            ], 200);
         } catch (ValidationException $e) {
             return response()->json([
                 'success' => false,
@@ -337,15 +351,16 @@ class ApiPelangganController extends Controller
             'datauser' => $pelanggan,
         ], 200);
     }
-    
-    public function edit_password(string $id, Request $request){
+
+    public function edit_password(string $id, Request $request)
+    {
         try {
             $request->validate([
                 'old_password' => 'required',
-                'new_password' => 'required',        
-                'new_password_confirmation' => 'required',        
+                'new_password' => 'required',
+                'new_password_confirmation' => 'required',
             ]);
-        
+
             // Lanjutkan dengan operasi lain jika validasi berhasil
         } catch (ValidationException $e) {
             return response()->json([
@@ -384,7 +399,114 @@ class ApiPelangganController extends Controller
                 'message' => 'Password lama tidak cocok!',
             ], 422);
         }
-        
+    }
+
+    public function getTransaksi($id_pelanggan)
+    {
+        $cek_data = Transaksi::where('id_pelanggan', $id_pelanggan)->first();
+
+        $transaksi = Transaksi::where('transaksi.id_pelanggan', $id_pelanggan)
+            ->join('pelanggan', 'transaksi.id_pelanggan', '=', 'pelanggan.id_pelanggan')
+            ->join('tagihan', 'transaksi.id_tagihan', '=', 'tagihan.id_tagihan');
+
+        if (empty($cek_data)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Belum ada transaksi',
+            ], 422);
+        } else {
+            $data = $transaksi
+                ->select(
+                    'transaksi.id_transaksi',
+                    'transaksi.resi_transaksi',
+                    'tagihan.jumlah_tagihan',
+                    'tagihan.status_tagihan',
+                )->get();
+
+            $formattedResult = $data->map(function ($item) {
+                return [
+                    'id_transaksi' => $item->id_transaksi,
+                    'resi' => $item->resi_transaksi,
+                    'total_tagihan' => number_format($item->jumlah_tagihan, 0, ',', '.'),
+                    'status_pembayaran' => $item->status_tagihan,
+                ];
+            });
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data berhasil ditemukan',
+                'data' => $formattedResult,
+            ], 200);
+        }
+    }
+
+    public function getDetailTransaksi($id_transaksi)
+    {
+        $transaksi = Transaksi::where('transaksi.id_transaksi', $id_transaksi)
+            ->join('pelanggan', 'transaksi.id_pelanggan', '=', 'pelanggan.id_pelanggan')
+            ->join('tagihan', 'transaksi.id_tagihan', '=', 'tagihan.id_tagihan')
+            ->join('pesanan', 'transaksi.id_transaksi', '=', 'pesanan.id_transaksi');
+
+        if (empty($transaksi)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data tidak ditemukan!',
+            ], 422);
+        } else {
+            $pesanan_awal = Pesanan::select('tanggal_pesanan', 'jumlah_bar', 'harga_pesanan',)
+                ->where('id_transaksi', $id_transaksi)->orderBy('tanggal_pesanan', 'asc')->first();
+            $pesanan_akhir = Pesanan::select('tanggal_pesanan', 'jumlah_bar', 'harga_pesanan',)
+                ->where('id_transaksi', $id_transaksi)->orderBy('tanggal_pesanan', 'desc')->first();
+            $pesanan = Pesanan::select('tanggal_pesanan', 'jumlah_bar', 'harga_pesanan',)
+                ->where('id_transaksi', $id_transaksi)->get();
+            Carbon::setLocale('id');
+            $formattedJumlahTagihan = number_format($pesanan_awal->harga_pesanan, 0, ',', '.');
+            $formattedTanggalJatuhTempo = Carbon::parse($pesanan_awal->tanggal_pesanan)->isoFormat('DD MMMM YYYY');
+
+            $formattedJumlahTagihanPesanan = number_format($pesanan_akhir->harga_pesanan, 0, ',', '.');
+            $formattedTanggalPesanan = Carbon::parse($pesanan_akhir->tanggal_pesanan)->isoFormat('DD MMMM YYYY');
+
+            $pesanan_akhir->tanggal_pesanan = $formattedTanggalPesanan;
+            $pesanan_akhir->harga_pesanan = $formattedJumlahTagihanPesanan;
+            $pesanan_awal->tanggal_pesanan = $formattedTanggalJatuhTempo;
+            $pesanan_awal->harga_pesanan = $formattedJumlahTagihan;
+
+            $formattedPesanan = $pesanan->map(function ($item) {
+                return [
+                    'tanggal_pesanan' => Carbon::parse($item->tanggal_pesanan)->isoFormat('DD MMMM YYYY'),
+                    'jumlah_bar' => $item->jumlah_bar,
+                    'harga_pesanan' => number_format($item->harga_pesanan, 0, ',', '.'),
+                ];
+            });
+
+            $data = $transaksi
+                ->select(
+                    'transaksi.resi_transaksi AS resi',
+                    'pelanggan.nama_perusahaan AS pelanggan',
+                    'pelanggan.no_hp',
+                    'pelanggan.alamat AS alamat',
+                    'tagihan.jumlah_tagihan AS total_tagihan',
+                    'pesanan.tanggal_pesanan',
+                    'pesanan.jumlah_bar',
+                    'pesanan.harga_pesanan',
+                )->first();
+
+            $formattedData = [
+                'resi' => $data->resi,
+                'pelanggan' => $data->pelanggan,
+                'no_hp' => $data->no_hp,
+                'alamat' => $data->alamat,
+                'pesanan_awal' => $pesanan_awal,
+                'pesanan_akhir' => $pesanan_akhir,
+                'pesanan' => $formattedPesanan
+            ];
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data berhasil ditemukan',
+                'data' => $formattedData,
+            ], 200);
+        }
     }
 
     private function hidePhoneNumber($phoneNumber)
@@ -392,51 +514,50 @@ class ApiPelangganController extends Controller
         // Menyembunyikan karakter kecuali 4 digit terakhir
         $visibleDigits = 4;
         $length = strlen($phoneNumber);
-    
+
         if ($length <= $visibleDigits) {
             return $phoneNumber;
         }
-    
+
         $hiddenPart = str_repeat('*', $length - $visibleDigits);
         $visiblePart = substr($phoneNumber, -$visibleDigits);
-    
+
         return $hiddenPart . $visiblePart;
     }
-    
+
     private function encryptEmail($email)
     {
         $emailParts = explode('@', $email);
-        
+
         if (count($emailParts) === 2) {
             $username = $emailParts[0];
             $domain = $emailParts[1];
-    
+
             // Enkripsi huruf di tengah
             $encryptedUsername = $this->encryptMiddle($username);
-    
+
             // Gabungkan kembali
             $encryptedEmail = $encryptedUsername . '@' . $domain;
-    
+
             return $encryptedEmail;
         }
-    
+
         return $email;
     }
-    
+
     private function encryptMiddle($text)
     {
         $length = strlen($text);
-    
+
         if ($length <= 2) {
             return $text;
         }
-    
+
         $start = substr($text, 0, 1);
         $end = substr($text, -1);
-    
+
         $middle = str_repeat('*', $length - 2);
-    
+
         return $start . $middle . $end;
     }
-    
 }
--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,8 @@ Route::middleware(['auth:sanctum', 'check.pelanggan'])->group(function () {
     Route::put('/pelanggan/update/alamat/{id}', [ApiPelangganController::class, 'edit_alamat']);    
     Route::put('/pelanggan/update/password/{id}', [ApiPelangganController::class, 'edit_password']);
     Route::get('/pelanggan/index/{id}', [ApiPembelianController::class, 'index_transaksi']);
+    Route::get('/pelanggan/transaksi/{id}', [ApiPelangganController::class, 'getTransaksi']);
+    Route::get('/pelanggan/detailtransaksi/{id}', [ApiPelangganController::class, 'getDetailTransaksi']);
     Route::get('/pembelian/belum_bayar', [ApiPembelianController::class, 'transaksi_belum_bayar']);
     Route::get('/pembelian/sudah_bayar', [ApiPembelianController::class, 'transaksi_sudah_bayar']);
     Route::post('/pelanggan/pembelian', [ApiPembelianController::class, 'create_transaksi']);
