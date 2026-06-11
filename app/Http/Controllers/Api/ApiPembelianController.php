5eafcf94f6ce7a8f2285717cbc6a622b11ce3d44
haasstt <nurafiifalmasazhr@gmail.com>
2026-06-11 22:49:40 +0700

    fix: bug get tagihan pelanggan

--- app/Http/Controllers/Api/ApiPembelianController.php
+++ app/Http/Controllers/Api/ApiPembelianController.php
@@ -613,25 +613,25 @@ class ApiPembelianController extends Controller
             ->where('status_tagihan', "Belum Bayar")
             ->first(); 
 
-        if (empty($pelanggan)) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Tidak ada tagihan',
-                'data' => $pelanggan,
-            ], 422);
-        }
-
-        $formattedJumlahTagihan = number_format($pelanggan->jumlah_tagihan, 0, ',', '.');
-        $formattedTanggalJatuhTempo = Carbon::parse($pelanggan->tanggal_jatuh_tempo)->isoFormat('DD MMMM YYYY');
-        $pelanggan->tanggal_jatuh_tempo = $formattedTanggalJatuhTempo;
-        $pelanggan->jumlah_tagihan = $formattedJumlahTagihan;
-
-        // if (!empty($pelanggan)) {
-        //     $formattedJumlahTagihan = number_format($pelanggan->jumlah_tagihan, 0, ',', '.');
-        //     $formattedTanggalJatuhTempo = Carbon::parse($pelanggan->tanggal_jatuh_tempo)->isoFormat('DD MMMM YYYY');
-        //     $pelanggan->tanggal_jatuh_tempo = $formattedTanggalJatuhTempo;
-        //     $pelanggan->jumlah_tagihan = $formattedJumlahTagihan;
+        // if (empty($pelanggan)) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Tidak ada tagihan',
+        //         'data' => $pelanggan,
+        //     ], 422); 
         // }
+
+        // $formattedJumlahTagihan = number_format($pelanggan->jumlah_tagihan, 0, ',', '.');
+        // $formattedTanggalJatuhTempo = Carbon::parse($pelanggan->tanggal_jatuh_tempo)->isoFormat('DD MMMM YYYY');
+        // $pelanggan->tanggal_jatuh_tempo = $formattedTanggalJatuhTempo;
+        // $pelanggan->jumlah_tagihan = $formattedJumlahTagihan;
+
+        if (!empty($pelanggan)) {
+            $formattedJumlahTagihan = number_format($pelanggan->jumlah_tagihan, 0, ',', '.');
+            $formattedTanggalJatuhTempo = Carbon::parse($pelanggan->tanggal_jatuh_tempo)->isoFormat('DD MMMM YYYY');
+            $pelanggan->tanggal_jatuh_tempo = $formattedTanggalJatuhTempo;
+            $pelanggan->jumlah_tagihan = $formattedJumlahTagihan;
+        }
 
         return response()->json([
             'success' => true,
